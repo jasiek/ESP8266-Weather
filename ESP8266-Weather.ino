73e41cc41d092278e7616eb91f6c576a2c52273c
Jan Szumiec <jan.szumiec@gmail.com>
2015-12-10 22:19:48 +0000

    Enabled ultra-low power mode.

--- ESP8266-Weather.ino
+++ ESP8266-Weather.ino
@@ -6,36 +6,45 @@
 #include "settings.h"
 
 Adafruit_BMP085 bmp;
+
+ADC_MODE(ADC_VCC);
+
 void setup() {
-  Serial.begin(115200);
-  Serial.println();
-  Serial.print("Connecting to network: ");
-  Serial.println(WIFI_SSID);
 
-  WiFi.begin(WIFI_SSID, WIFI_PASS);
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-    delay(500);
-  }
-
-  Serial.println(WiFi.localIP());
-  if (!bmp.begin()) {
-    Serial.println("Can't initialize sensor");
-    while(1);
-  }
 }
 
 void loop() {
+    Serial.begin(115200);
+    Serial.println();
+  
+   if (!bmp.begin()) {
+     Serial.println("Can't initialize sensor");
+     while(1);
+   }
+
+    Serial.print("Connecting to network: ");
+    Serial.println(WIFI_SSID);
+
+    WiFi.begin(WIFI_SSID, WIFI_PASS);
+    while (WiFi.status() != WL_CONNECTED) {
+      Serial.print(".");
+      delay(500);
+    }
+    Serial.println(WiFi.localIP());
+
     float temp = bmp.readTemperature();
     float press = bmp.readPressure() / 100;
+    float vcc = ESP.getVcc() / 1000;
 
-    String url = String(DATA_PATH) + "&temp=" + String(temp) + "&pressure=" + String(press);
+    String url = String(DATA_PATH) + "&temp=" + String(temp) + "&pressure=" + String(press) + "&vcc=" + String(vcc);
     WiFiClientSecure client;
 
     Serial.print("Temp: ");
     Serial.print(temp);
     Serial.print(" Pressure: ");
-    Serial.println(press);
+    Serial.print(press);
+    Serial.print(" Voltage: ");
+    Serial.println(vcc); 
     
     if (client.connect(DATA_HOST, DATA_PORT)) {
       client.print(String("GET ") + url + " HTTP/1.1\r\n" +
@@ -57,5 +66,6 @@ void loop() {
     } else {
       Serial.println("Could not connect, will try later");
     }
-    delay(300000);
+    ESP.deepSleep(600 * 1000000);
+    delay(1000);
 }
