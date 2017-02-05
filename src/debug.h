4dcda99fcac5dfd9dc1c3ee0f1276784554d78c3
Jan Szumiec <jan.szumiec@gmail.com>
2017-02-05 12:14:30 +0100

    Improved tracing/debugging code.

--- src/debug.h
+++ src/debug.h
@@ -1,6 +1,12 @@
 #ifndef _DEBUG_H
 #define _DEBUG_H
 
-#define DEBUG(x) Serial.println(x)
+#include <Arduino.h>
 
+#define DEBUG(...) debug(__FILE__, __LINE__, __VA_ARGS__)
+
+void debug(const char *, int, const char *);
+void debug(const char *, int, const char *, const char *);
+void debug(const char *, int, String &);
+void debug(const char *, int, String &, String &);
 #endif
